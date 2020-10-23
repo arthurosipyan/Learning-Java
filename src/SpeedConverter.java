69b859f49dd6c51a6edfb104f2e6b50bcdac51a0
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-10-23 08:19:33 -0400

    Initial commit

---
+++ src/SpeedConverter.java
@@ -0,0 +1,28 @@
+public class SpeedConverter {
+    // write your code here
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            return -1;
+        }
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        if(kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+        }
+        else{
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
+}
