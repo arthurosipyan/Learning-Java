843219de6f39e14cae2b517fe108e2d285493020
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-10-23 08:39:39 -0400

    created EqualSumChecker and TeenNumberChecker

---
+++ src/TeenNumberChecker.java
@@ -0,0 +1,16 @@
+public class TeenNumberChecker {
+
+    public static boolean hasTeen(int x, int y, int z){
+        return 13 <= x && x <= 19 || 13 <= y && y <= 19 || 13 <= z && z <= 19;
+    }
+
+    public static boolean isTeen(int age){
+        return age >= 13 && age <= 19;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+}
