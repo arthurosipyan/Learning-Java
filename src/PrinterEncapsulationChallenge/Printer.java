f5b5ea457376614ef38af91f670be91b14bdf760
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-10-26 20:00:41 -0400

    printer encapsulation challenge

---
+++ src/PrinterEncapsulationChallenge/Printer.java
@@ -0,0 +1,42 @@
+package PrinterEncapsulationChallenge;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel >= 1 && tonerLevel <= 100 ? tonerLevel : -1;
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner(int tonerAmount){
+        if(tonerAmount > 0 && tonerAmount <= 100){
+            if(this.tonerLevel + tonerAmount > 100){
+                return -1;
+            }
+            this.tonerLevel += tonerAmount;
+            return this.tonerLevel;
+        }
+        return -1;
+    }
+
+    public int printPages(int pages){
+        int pageesToPrint = pages;
+        if(this.duplex){
+            pageesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted +=pageesToPrint;
+        return pageesToPrint;
+    }
+
+    public void setPagesPrinted(int pagesPrinted) {
+        this.pagesPrinted = pagesPrinted;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
