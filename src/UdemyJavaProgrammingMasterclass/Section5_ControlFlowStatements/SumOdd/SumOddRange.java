0b393a4f0bd1d434f8f08ee3be762b382ba1069e
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-11-09 22:08:52 -0500

    Create SumOddRange.java

---
+++ src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/SumOdd/SumOddRange.java
@@ -0,0 +1,27 @@
+package UdemyJavaProgrammingMasterclass.Section5_ControlFlowStatements.SumOdd;
+
+public class SumOddRange {
+    public static boolean isOdd(int number){
+        return number > 0 && number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end){
+        if(end >= start && start > 0){
+            int sum = 0;
+            for(int i = start; i <= end; i++){
+                sum += isOdd(i) ? i : 0;
+            }
+            return sum;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        sumOdd(1, 100);
+        sumOdd(-1, 100);
+        sumOdd(100, 100);
+        sumOdd(13, 13);
+        sumOdd(100, -100);
+        sumOdd(100, 1000);
+    }
+}
