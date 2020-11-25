8fba9a51d235145ad2d882c587af57df8f10ef6f
arthurosipyan <arthurosipyan@outlook.com>
2020-11-24 21:04:53 -0500

    Solution to PerfectNumber method

--- src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/PerfectNumber/PerfectNumber.java
+++ src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/PerfectNumber/PerfectNumber.java
@@ -1,4 +1,25 @@
 package UdemyJavaProgrammingMasterclass.Section5_ControlFlowStatements.PerfectNumber;
 
 public class PerfectNumber {
+    public static boolean isPerfectNumber(int number){
+        if(number < 1){
+            return false;
+        }
+
+        int sum = 0;
+
+        for(int i = 1; i < number; i++){
+            if(number % i == 0){
+                sum += i;
+            }
+        }
+        return number == sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
 }
