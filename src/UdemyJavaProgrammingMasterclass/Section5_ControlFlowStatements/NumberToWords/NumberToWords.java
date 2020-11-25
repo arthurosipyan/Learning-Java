7a1e4b099f2b50ac264ae1d8a11ab2fca948f637
arthurosipyan <arthurosipyan@outlook.com>
2020-11-24 21:10:11 -0500

    Created NumberToWords challenge. Need solution...

---
+++ src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/NumberToWords/NumberToWords.java
@@ -0,0 +1,15 @@
+package UdemyJavaProgrammingMasterclass.Section5_ControlFlowStatements.NumberToWords;
+
+public class NumberToWords {
+    public static void numberToWords(int number){
+        if(number < 0){
+            System.out.println("Invalid Value.");
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
