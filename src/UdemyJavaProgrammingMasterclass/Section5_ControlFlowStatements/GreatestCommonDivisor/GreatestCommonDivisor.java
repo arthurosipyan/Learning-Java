25c07f848655a1f63507258814ca6216c2b4bab0
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-12-02 19:57:01 -0500

    gcd

--- src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/GreatestCommonDivisor/GreatestCommonDivisor.java
+++ src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/GreatestCommonDivisor/GreatestCommonDivisor.java
@@ -1,20 +1,19 @@
 package UdemyJavaProgrammingMasterclass.Section5_ControlFlowStatements.GreatestCommonDivisor;
 
 public class GreatestCommonDivisor {
-    public static int getGreatestCommonDivisor(int first, int second){
-        if(first < 10 || second < 10){
+    public static int getGreatestCommonDivisor(int x, int y){
+        if(x < 10 || y < 10){
             return -1;
         }
-
-        int gcd = 1;
-
-        for(int i = 1; i <= first && i <= second; i++){
-            if(first % i == 0 && second % i == 0){
-                gcd = i;
+        int i = 1;
+        while(x % i == 0){
+            if(y % i == 0){
+                i++;
             }
         }
-        return gcd;
+        return i;
     }
+
     public static void main(String[] args) {
         System.out.println(getGreatestCommonDivisor(25, 15));
         System.out.println(getGreatestCommonDivisor(12, 30));
--- src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/NumberToWords/NumberToWords.java
+++ src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/NumberToWords/NumberToWords.java
@@ -2,14 +2,95 @@ package UdemyJavaProgrammingMasterclass.Section5_ControlFlowStatements.NumberToW
 
 public class NumberToWords {
     public static void numberToWords(int number){
-        if(number < 0){
-            System.out.println("Invalid Value.");
+        int places = getDigitCount(number);
+
+        if(places < 1){
+            System.out.println("Invalid Value");
         }
 
+        int reversedNumber = reverse(number);
 
+        while (places > 0){
+            places--;
+            int current = reversedNumber % 10;
+
+            switch(current){
+                case 0:
+                    System.out.println("Zero");
+                    break;
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+                default:
+            }
+
+            reversedNumber /= 10;
+        }
+    }
+
+    public static int reverse(int number){
+        int reverse = 0;
+
+        while(number != 0){
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+
+        return reverse;
+    }
+
+    public static int getDigitCount(int number){
+        if(number < 0){
+            return -1;
+        }
+
+        int count = 0;
+
+        while(number > 0){
+            count++;
+            number /= 10;
+        }
+
+        return count > 0 ? count : 1;
     }
 
     public static void main(String[] args) {
+        System.out.println(getDigitCount(0));
+        System.out.println(getDigitCount(123));
+        System.out.println(getDigitCount(-12));
+        System.out.println(getDigitCount(5200));
 
+        System.out.println(reverse(-121));
+        System.out.println(reverse(2121));
+        System.out.println(reverse(4321));
+
+        numberToWords(123);
+        numberToWords(1010);
+        numberToWords(1000);
+        numberToWords(-12);
     }
 }
