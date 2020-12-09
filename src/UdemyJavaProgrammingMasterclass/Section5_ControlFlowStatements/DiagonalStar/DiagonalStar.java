c8c127d72407d54b38eb861c7c9e7f85b03a2690
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-12-08 23:43:31 -0500

    progress on DiagonalStar challenge

---
+++ src/UdemyJavaProgrammingMasterclass/Section5_ControlFlowStatements/DiagonalStar/DiagonalStar.java
@@ -0,0 +1,15 @@
+package UdemyJavaProgrammingMasterclass.Section5_ControlFlowStatements.DiagonalStar;
+
+public class DiagonalStar {
+    public static void printSquareStar(int number){
+        if(number < 5){
+            System.out.println("Invalid Value");
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
